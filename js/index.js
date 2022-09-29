431da958bf66dc0ee8b87723294a6658ab2f697b
ridemetothemoon12 <97011890+ridemetothemoon12@users.noreply.github.com>
2022-09-29 14:56:42 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $('document').ready(function() {
     let page = new fullpage(".fullpage", {
+        licenseKey: "603D3EBB-D1B84561-B40D98B6-7A87C3D7",
         navigation:true,
         navigationTooltips: ["HOME", "ROOM", "OFFER", "DINING", "PROMOTION","MAP"],
         scrollOverflow: true,
@@ -28,75 +29,25 @@ $('document').ready(function() {
         }
     })
 
-    const swiper = new Swiper(".swiper_main", {
-        effect: "slide",
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false
-        },
-        loop: true
+    $(".location").mouseenter(function() {
+        $('.waypoint').stop().animate({
+            left: "100%"
+        }, 1000)
     })
-    const swiper_1 = new Swiper(".swiper_content_1", {
-        effect: "slide",
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false
-        },
-        pagination: {
-            el: ".swiper-pagination",
-        }
-    })
-    const swiper_2 = new Swiper(".swiper_content_2", {
-        effect: "fade",
-        slidesPerView: "auto",
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-        }
+    $(".location").mouseleave(function() {
+        $('.waypoint').stop().animate({
+            left: "70%"
+        }, 1000)
     })
 
-    swiper.on("transitionEnd", function(e){
-        var slideIndex = swiper.realIndex;
-        $('.line').removeClass("on");
-        $('.line').eq(slideIndex).addClass("on");
-    });
-    $(".line").click(function() {
-        let i = $(this).index();
-        swiper.slideTo(i+1)
+    $(".nav").mouseenter(function() {
+        $('.nav_expand').stop().animate({
+            height: "230px"
+        }, 300)
     })
-    
-    swiper_1.on("transitionEnd", function(e) {
-        let textArr = ["비스타 듀플렉스","파크힐 듀플렉스","남대문 듀플렉스"];
-        let textArr_sub = ["주방과 침실, 응접과 집무실, 자쿠지 등 최고 시설이 갖춰진 공간으로 최상의 서비스와 품격을 느끼실 수 있습니다.",
-        "복층 구조로 남산을 바라보고 있어 자연의 향취를 마음껏 즐길 수 있는 공간입니다.",
-        "탁 트인 통 유리를 통해 사계절의 변화를 느낄 수 있는 남산을 한눈에 바라볼 수 있으며, 내려다 보이는 서울 시내의 야경은 특히 일품입니다."]
-        var slide_1_Index = swiper_1.realIndex;
-        console.log(slide_1_Index);
-        $(".item_left .text h3").text(textArr[slide_1_Index]);
-        $(".item_left .text p").text(textArr_sub[slide_1_Index]);
-    })
-
-    $(".offer .content li").click(function() {
-        let back_img = ["linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(../images/meeting.jpg)",
-    "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(../images/casino.jpg)",
-    "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(../images/spa.jpg)" ,
-    "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(../images/lounge.jpg)", 
-    "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(../images/pool.png)"]
-        let j = $(this).index();
-        $(".offer").css("background-image",back_img[j]);
-        $(".offer .content li").removeClass("on");
-        $(this).addClass("on");
-
-        $(".description .text").removeClass("on");
-        $(".description .text").eq(j).addClass("on");
-    })
-
-    swiper_2.on("transitionEnd", function(e) {
-        var slide_2_Index = swiper_2.realIndex;
-        $(".promotion .text .content_wrap").removeClass("on");
-        $(".promotion .text .content_wrap").eq(slide_2_Index).addClass("on");
+    $(".nav").mouseleave(function() {
+        $('.nav_expand').stop().animate({
+            height: "0"
+        }, 300)
     })
 })
